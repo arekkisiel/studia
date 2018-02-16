328f7028d1c207358a8d0f364b71b911da5503ca
Arkadiusz Kisiel <arekkisiel92@gmail.com>
2018-02-16 21:35:26 +0100

    Reimplemented SyntaxAnalyzer - TOP DOWN algorithm.

--- SyntaxAnalyzer/src/Utils.java
+++ SyntaxAnalyzer/src/Utils.java
@@ -1,15 +1,24 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class Utils {
-    private static int INITIAL_STATE = 0;
-    private static int ACCEPTED_STATE1 = 2;
-    private static int ACCEPTED_STATE2 = 4;
-    private static int ACCEPTED_STATE3 = 6;
-    private static int ACCEPTED_STATE4 = 10;
-    private static int ACCEPTED_STATE5 = 12;
-    private static int INVALID_STATE = 999;
+    private static String PRODUCTION;
+    private static String INPUT;
+    /*PRODUCTIONS
+    S = AFAE | (S)E
+    A = CB | (-CB)
+    B = ,C |  $
+    C = 0D | GD
+    D = 0D | GD | $
+    E = FS | $
+    F = +|-|*|/|^
+    G = 1|2|3|4|5|6|7|8|9
+    */
+
     public static Scanner loadData() {
         Scanner read = null;
         try {
@@ -21,236 +30,174 @@ public class Utils {
         return read;
     }
 
-    public static Boolean checkGrammar(String series) {
-        int currentState = INITIAL_STATE;
-        for(int index = 0; index < series.length(); index++ ) {
-            String currentInput = series.substring(index, index + 1);
-            switch (currentState) {
-                case 0:
-                    if (isMinus(currentInput)) {
-                        currentState = 1;
-                        break;
-                    }
-                    if (isNumber(currentInput)) {
-                        if(Integer.parseInt(currentInput) > 0) {
-                            currentState = ACCEPTED_STATE1;
-                            break;
-                        }
-                        currentState = 5;
-                        break;
-                    }
-                    if(isOpenParenthesis(currentInput)){
-                        currentState = 11;
-                        break;
-                    }
-                    currentState = INVALID_STATE;
-                    break;
-                case 1:
-                    if (isNumber(currentInput)) {
-                        if(Integer.parseInt(currentInput) > 0) {
-                            currentState = ACCEPTED_STATE1;
-                            break;
-                        }
-                        currentState = 13;
-                        break;
-                    }
-                    currentState = INVALID_STATE;
-                    break;
-                case 2:
-                    if (isNumber(currentInput)) {
-                        currentState = ACCEPTED_STATE1;
-                        break;
-                    }
-                    if (isOperator(currentInput)) {
-                        currentState = 3;
-                        break;
-                    }
-                    if (isDot(currentInput)) {
-                        currentState = 7;
-                        break;
-                    }
-                    currentState = INVALID_STATE;
-                    break;
-                case 3:
-                    if (isNumber(currentInput)) {
-                        if(Integer.parseInt(currentInput) > 0) {
-                            currentState = ACCEPTED_STATE2;
-                            break;
-                        }
-                        currentState = ACCEPTED_STATE3;
-                        break;
-                    }
-                    if(isOpenParenthesis(currentInput)){
-                        currentState = 11;
-                        break;
-                    }
-                    currentState = INVALID_STATE;
-                    break;
-                case 4:
-                    if(isNumber(currentInput)) {
-                        currentState = ACCEPTED_STATE2;
-                        break;
-                    }
-                    if(isOperator(currentInput)) {
-                        currentState = 3;
-                        break;
-                    }
-                    if(isDot(currentInput)){
-                        currentState = 9;
-                        break;
-                    }
-                    currentState = INVALID_STATE;
-                    break;
-                case 5:
-                    if(isOperator(currentInput)){
-                        currentState = 3;
-                        break;
-                    }
-                    if(isDot(currentInput)){
-                        currentState = 7;
-                        break;
-                    }
-                    currentState = INVALID_STATE;
-                    break;
-                case 6:
-                    if(isOperator(currentInput)) {
-                        currentState = 3;
-                        break;
-                    }
-                    if(isDot(currentInput)){
-                        currentState = 9;
-                        break;
-                    }
-                    currentState = INVALID_STATE;
-                    break;
-                case 7:
-                    if(isNumber(currentInput)){
-                        currentState = 8;
-                        break;
-                    }
-                    currentState = INVALID_STATE;
-                    break;
-                case 8:
-                    if(isNumber(currentInput)){
-                        break;
-                    }
-                    if(isOperator(currentInput)){
-                        currentState = 3;
-                        break;
-                    }
-                    currentState = INVALID_STATE;
-                    break;
-                case 9:
-                    if(isNumber(currentInput)){
-                        currentState = ACCEPTED_STATE4;
-                        break;
-                    }
-                    currentState = INVALID_STATE;
-                    break;
-                case 10:
-                    if(isNumber(currentInput)){
-                        break;
-                    }
-                    if(isOperator(currentInput)){
-                        currentState = 3;
-                        break;
-                    }
-                    currentState = INVALID_STATE;
-                    break;
-                case 11:
-                    int subseriesEndIndex = findClosingParenthesis(series.substring(index)) + index;
-                    if(subseriesEndIndex > 0) {
-                        if (checkGrammar(series.substring(index, subseriesEndIndex))) {
-                            index += (subseriesEndIndex - 1);
-                            currentState = ACCEPTED_STATE5;
-                            break;
-                        }
-                    }
-                    currentState = INVALID_STATE;
-                    break;
-                case 12:
-                    if(isOperator(currentInput)){
-                        currentState = 3;
-                        break;
-                    }
-                    if(isOpenParenthesis(currentInput)){
-                        currentState = 11;
-                        break;
-                    }
-                    currentState = INVALID_STATE;
-                    break;
-                case 13:
-                    if(isDot(currentInput)){
-                        currentState = 7;
-                        break;
-                    }
-                    currentState = INVALID_STATE;
-                    break;
-                case 999:
-                    break;
-            }
+
+    public static boolean checkGrammar(String input){
+        int iterator = 0;
+        //initialization
+        INPUT = input;
+        PRODUCTION = "S";
+        while(PRODUCTION.length() > 0){
+            if(INPUT.length() == 0)
+                INPUT = "$";
+            String partial_production = (fitProduction(first(INPUT), first(PRODUCTION)));
+            if(partial_production.equals("INVALID"))
+                return false;
+            PRODUCTION = partial_production.concat(PRODUCTION.substring(1));
+            Boolean dropped;
+            do {
+                dropped = dropIfEqual();
+            }while(dropped == true && PRODUCTION.length() > 0 && INPUT.length() > 0);
         }
-        if(currentState == ACCEPTED_STATE1 || currentState == ACCEPTED_STATE2 || currentState == ACCEPTED_STATE3 || currentState == ACCEPTED_STATE4 || currentState == ACCEPTED_STATE5)
-            return true;
-        return false;
-    }
-
-    private static int findClosingParenthesis(String series) {
-        if(series.indexOf("(") < series.indexOf(")")){
-            int countOpen = 0;
-            String tmpSeries = series;
-            while(tmpSeries.contains("(")){
-                countOpen++;
-                int tmpIndex = tmpSeries.indexOf("(");
-                tmpSeries = tmpSeries.substring(tmpIndex+1);
-            }
-            int finalIndex = 0;
-            tmpSeries = series;
-            for(int index = 0; index <= countOpen; index++){
-                int tmpIndex = tmpSeries.indexOf(")");
-                finalIndex += tmpIndex;
-                if(tmpIndex == 0)
-                    finalIndex++;
-                tmpSeries = tmpSeries.substring(tmpIndex+1);
-            }
-            return finalIndex;
-        }
-        else
-            return series.indexOf(")");
-    }
-
-    private static boolean isMinus(String currentInput) {
-        if(currentInput.contains("-"))
-            return true;
-        return false;
-    }
-
-    private static boolean isOperator(String currentInput) {
-        if(currentInput.contains("-") || currentInput.contains("+") || currentInput.contains("*") || currentInput.contains("/") || currentInput.contains("^"))
-            return true;
-        return false;
-    }
-
-    private static boolean isNumber(String input) {
-        try {
-            Integer.parseInt(input);
-            return true;
-        }
-        catch (NumberFormatException e){
+        if(PRODUCTION.length() == 0 && INPUT.length() > 0)
             return false;
+        return true;
+    }
+
+    private static boolean dropIfEqual() {
+        if(first(INPUT).equals(first(PRODUCTION))){
+            INPUT = INPUT.substring(1);
+            PRODUCTION = PRODUCTION.substring(1);
+            return true;
         }
+        else if(first(PRODUCTION).equals("$")) {
+            PRODUCTION = PRODUCTION.substring(1);
+            return true;
+        }
+        return false;
     }
 
-    private static boolean isDot(String currentInput) {
-        if(currentInput.contains("."))
+    private static String fitProduction(String input, String production){
+        switch (production){
+    /*PRODUCTIONS
+    S=AEJ|(S)J
+    A=CB|(K |-CB
+    B=.I|$
+    C=0|GD
+    D=0D|GD|$
+    E= FLE|$
+    F = +|-|*|/|^
+    G = 1|2|3|4|5|6|7|8|9
+    H=0H|GH|$
+    I=0H|GH
+    J=FS|$
+    K=-CB)|S)
+    L=CB|(K
+    */
+            case "S":
+                if(input.contains("("))
+                    return "(S)J";
+                return "AEJ";
+            case "A":
+                if(input.contains("("))
+                    return "(K";
+                if(input.contains("-"))
+                    return "-CB";
+                return "CB";
+            case "B":
+                if(input.contains("."))
+                    return ".I";
+                return "$";
+            case "C":
+                if(input.contains("0"))
+                    return "0";
+                return "GD";
+            case "D":
+                if(input.contains("0"))
+                    return "0D";
+                if(isDigit(input))
+                    return "GD";
+                return "$";
+            case "E":
+                if(isOperator(input))
+                    return "FLE";
+                return "$";
+            case "F":
+                if(input.contains("+"))
+                    return "+";
+                if(input.contains("-"))
+                    return "-";
+                if(input.contains("*"))
+                    return "*";
+                if(input.contains("/"))
+                    return "/";
+                if(input.contains("^"))
+                    return "^";
+                break;
+            case "G":
+                if(input.contains("1"))
+                    return "1";
+                if(input.contains("2"))
+                    return "2";
+                if(input.contains("3"))
+                    return "3";
+                if(input.contains("4"))
+                    return "4";
+                if(input.contains("5"))
+                    return "5";
+                if(input.contains("6"))
+                    return "6";
+                if(input.contains("7"))
+                    return "7";
+                if(input.contains("8"))
+                    return "8";
+                if(input.contains("9"))
+                    return "9";
+                break;
+            case "H":
+                if(input.contains("0"))
+                    return "0H";
+                if(isDigit(input))
+                    return "GH";
+                return "$";
+            case "I":
+                if(input.contains("0"))
+                    return "0H";
+                if(isDigit(input))
+                    return "GH";
+                break;
+            case "J":
+                if(isOperator(input))
+                    return "FS";
+                return "$";
+            case "K":
+                if(input.contains("-"))
+                    return "-CB)";
+                return "S)";
+            case "L":
+                if(input.contains("("))
+                    return "(K";
+                return "CB";
+        }
+        //Throw ERROR, INPUT does not fit to the GRAMMAR
+        return "INVALID";
+    }
+
+    private static String first(String input) {
+        return input.substring(0, 1);
+    }
+
+
+    //CHECK_TERMINALS
+    private static boolean isZero (String input) {
+        if(input.contains("0"))
             return true;
         return false;
     }
 
-    private static boolean isOpenParenthesis(String currentInput) {
-        if(currentInput.contains("("))
+    private static boolean isDigit (String input) {
+        List<String> digits = Arrays.asList("1", "2", "3", "4", "5", "6", "7", "8", "9");
+        if(digits.contains(input))
             return true;
         return false;
     }
+
+    private static boolean isOperator (String input) {
+        List<String> operators = Arrays.asList("-", "+", "*", "/", "^");
+        if(operators.contains(input))
+            return true;
+        return false;
+    }
+
+
 }
 
